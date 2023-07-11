26ec752550feb6cf21ee0455f1f6be96b439f821
Eva Kemp <eva.j.kemp@gmail.com>
2023-07-11 14:45:43 -0700

    test pass '

--- PingPong.Tests/ModelTests/NumTransformTests.cs
+++ PingPong.Tests/ModelTests/NumTransformTests.cs
@@ -1,30 +1,63 @@
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using PingPong.Models;
+// using System.Collections.Generic;
+// using System;
+
+// namespace PingPong.Tests
+// {
+//   [TestClass]
+//   public class NumTransformTests  //: IDisposable
+//   {
+//    // public void Dispose()
+//    // {
+//    //   NumTransform.ClearAll();
+//   //  }
+//     [TestMethod]
+
+//     public void
+//     Output_ReturnsUserNumWithPingPong()
+//     {
+//         int userNum = 3;
+//         NumTransform newNumTransform = new NumTransform();
+
+//         int result = newNumTransform.userNum;
+//         Assert.AreEqual(userNum.ToString(), result);
+
+//     }
+    
+//     }
+
+//   }
+
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PingPong.Models;
-using System.Collections.Generic;
-using System;
 
 namespace PingPong.Tests
 {
-  [TestClass]
-  public class NumTransformTests  //: IDisposable
-  {
-   // public void Dispose()
-   // {
-   //   NumTransform.ClearAll();
-  //  }
-    [TestMethod]
-
-    public void
-    Output_ReturnsUserNumWithPingPong()
+    [TestClass]
+    public class NumTransformTests
     {
-        int userNum = 3;
-        NumTransform newNumTransform = new NumTransform();
+        [TestMethod]
+        public void Output_ReturnsUserNumWithPingPong()
+        {
+            int userNum = 2;
+            NumTransform newNumTransform = new NumTransform { UserNum = userNum };
 
-        int result = newNumTransform.userNum;
-        Assert.AreEqual(userNum, result);
+            string result = newNumTransform.Output();
 
+            Assert.AreEqual(userNum.ToString(), result);
+        }
+
+         [TestMethod]
+        public void Output_ReturnsUserNum3WithPing()
+        {
+            int userNum = 3;
+            NumTransform newNumTransform = new NumTransform { UserNum = userNum };
+
+            string result = newNumTransform.Output();
+
+            Assert.AreEqual(userNum.ToString(), result);
+        }
     }
-    
-    }
-
-  }
+}
