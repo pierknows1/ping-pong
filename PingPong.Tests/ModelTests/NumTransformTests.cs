642a4e1fde7a3c449d58f738aa6a42836bbd35c0
Eva Kemp <eva.j.kemp@gmail.com>
2023-07-11 14:29:48 -0700

    test fail

--- PingPong.Tests/ModelTests/NumTransformTests.cs
+++ PingPong.Tests/ModelTests/NumTransformTests.cs
@@ -6,19 +6,19 @@ using System;
 namespace PingPong.Tests
 {
   [TestClass]
-  public class NumTransformTests : IDisposable
+  public class NumTransformTests  //: IDisposable
   {
-    public void Dispose()
-    {
-      NumTransform.ClearAll();
-    }
+   // public void Dispose()
+   // {
+   //   NumTransform.ClearAll();
+  //  }
     [TestMethod]
 
     public void
     Output_ReturnsUserNumWithPingPong()
     {
         int userNum = 3;
-        NumTransform newNumTransform = new NumTransform (userNum);
+        NumTransform newNumTransform = new NumTransform();
 
         int result = newNumTransform.userNum;
         Assert.AreEqual(userNum, result);
