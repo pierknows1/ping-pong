c6c7747e34ac2dfd848625a8a7568479f229eb9b
Eva Kemp <eva.j.kemp@gmail.com>
2023-07-12 09:42:21 -0700

    wip

--- PingPong/Models/NumTransform.cs
+++ PingPong/Models/NumTransform.cs
@@ -7,123 +7,47 @@ namespace PingPong.Models
     public class NumTransform
     {
         public int UserNum { get; set; }
-
-        // public static List<NumTransform>
-        // _instances = new List<NumTransform> { };
-        public string Output()
+        public string Output(int UserNum)
         {
-            if (UserNum % 3 == 0 && UserNum % 5 == 0)
+            List<string> numbers = new List<string>();
+            for (int index = 0; index <= UserNum; index++)
             {
-                return "ping pong";
+                if (index % 3 == 0 && index % 5 == 0)
+                {
+                    numbers.Add("ping pong");
+                }
+                else if (index % 3 == 0)
+                {
+                    numbers.Add("ping");
+                }
+                else if (index % 5 == 0)
+                {
+                    numbers.Add("pong");
+                }
+                else
+                {
+                    numbers.Add(index.ToString());
+                }
             }
-            else if (UserNum % 3 == 0)
-            {
-                return "ping";
-            }
-            else if (UserNum % 5 == 0)
-            {
-                return "pong";
-            }
-            else
-            {
-                return UserNum.ToString();
-            }
+
+            return numbers.ToString();
         }
         public NumTransform(int userNum)
         {
             UserNum = userNum;
-           // _instances.Add(this);
-        }
-
-        public string NumLoop(string UserNum) 
-        {
-            Output();
-            
-            // string[] theEntireArray = {};
-            // for (int index = 0; index < theEntireArray.Length; index++)
-            // {
-            //     Console.WriteLine($"{index}: {theEntireArray[index]}");
-            // }
         }
     }
-
 }
 
 
-//this is for a commit
-
-// planetLeft (){
-//   let left = this.expectancy - this.earthAge;
-
-//   if (left > 0) {
-//     this.left = `you have ${left} years to live.`
-//   } else if (left < 0 ){
-//     this.left = `You have lived ${Math.abs(left)} past your expectancy.`
-//   } else {
-//     this.left = `You have reached your life expectancy.`;
-//   }
-//   return this.left;
-//   }
-// }
-
-// test('should return years lived past expectancy on mercury', () => {
-//             let planet = new Age (28, 38);
-//             planet.mercury();
-//             expect(planet.planetLeft()).toEqual("You have lived 79 past your expectancy.")
-//           });
-
-//pierknows1 pierknows@gmail.com 
-
-
-//ESC18 <ponderosaprintingus@gmail.com>
 
 
 
-// Business Logic
-// let arr = [];
-
-// function arraySave(savedValue) {
-//     arr.push(savedValue);
-//     display(arr);
-// }
-
-// function tester (index) {
-//     if (index.toString().includes("3") === true) {
-//         index = " Won't you be my Neighbor? "
-//         let savedValue = index;
-//         arraySave(savedValue);
-//     } else if (index.toString().includes("2") === true) {
-//         index = " Beep";
-//         let savedValue = index;
-//         arraySave(savedValue);
-//     } else if (index.toString().includes("1") === true) {
-//         index = " Boop ";
-//         let savedValue = index;
-//         arraySave(savedValue);
-//     } else {
-//         let savedValue = index;
-//         arraySave(savedValue);
-//     }
-// }
 
 
 
-// UI Logic
-// function counter (event) {
-//     arr = [];
-//     event.preventDefault();
-//     let value = document.getElementById("input").value; 
-//     for (let index = 0; index <= value; index++) {
-//         tester(index);
-//     }
-// }
 
-// window.onload = function () {
-//     let form = document.getElementById("form");
-//     form.addEventListener("submit",counter);
-// }
 
-// function display(arr) {
-//     let output = document.querySelector("p");
-//     output.innerHTML = arr.toString();
-// }
+
+
+
--- PingPong/Program.cs
+++ PingPong/Program.cs
@@ -13,11 +13,11 @@ namespace PingPong
             Console.WriteLine("We'll return a list of numbers and messages based off your inputted number.");
             Console.WriteLine("Enter a number below:");
             string UserNum = Console.ReadLine();
-            // new NumTransform(UserNum);
 
             NumTransform OverallOutput = new NumTransform(int.Parse(UserNum));
 
-            string result = OverallOutput.Output();
+            // string numbers = OverallOutput.NumLoop(int.Parse(UserNum));
+            string result = OverallOutput.Output(int.Parse(UserNum));
             Console.WriteLine(result);
         }
     }
